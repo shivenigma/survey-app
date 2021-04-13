d110d282d34c3a6ddeee34d0f3beb962419d2e0a
Vignesh M <ms.vignesh31@gmail.com>
2021-04-14 01:34:41 +0530

    Included the JS function to replace image SVGs to inline onLoad.
    Now it'll let us theme our SVG using CSS as if it is inline and we can save the SVGs as files and use it without polluting the template.
    
    This is best of both worlds for icons. If you're using a lot of SVG on a page this might include some overhead. That's the reason I don't want to write it as a global script that converts all SVG.
    
    This script can be used sparingly on places only when it is required.

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+function replaceWithInlineSVG(image) {
+    fetch(image.src).then(function(response) {
+        return response.text();
+    }).then(function(response){
+            const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(response, "text/xml");
+        // Get the SVG tag, ignore the rest
+        let svg = xmlDoc.getElementsByTagName('svg')[0];
+        // Add replaced image's ID to the new SVG
+        if(typeof image.id !== 'undefined') {
+            svg.setAttribute('id', image.id);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof image.className !== 'undefined') {
+            svg.setAttribute('class', image.className);
+        }
+        // Remove any invalid XML tags as per http://validator.w3.org
+        svg.removeAttribute('xmlns:a');
+        // if the viewport is not set the SVG wont't scale.
+        if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+            svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
+        }
+        // Replace image with the generated SVG
+        image.parentNode.replaceChild(svg, image);
+    })
+}
